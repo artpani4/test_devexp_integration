4e0e814dd6043478c79fa6a1d20e456b8a7fc8bc
Artem <artp80@gmail.com>
2025-04-11 05:26:02 +0700

    input from dland

--- cli.ts
+++ cli.ts
@@ -1,5 +1,5 @@
 import { Command } from "jsr:@cliffy/command@1.0.0-rc.7";
-import { Input } from "jsr:@cliffy/prompt@1.0.0-rc.7";
+import { Input } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 
 import { useFileFlag } from "./alias/utils.ts";
 import { loadFromFile, recordUsageLocal, saveToFile } from "./alias/file.ts";
